b96313efecbf569a1acbdf507c2a47215da8ddd8
Mark Taylor <mark.taylor@betfred.com>
2017-02-15 20:02:22 +0000

    Step 1 - Added implementation for multiple order types

--- src/CafeX.OrderCalculator/OrderCalculator.cs
+++ src/CafeX.OrderCalculator/OrderCalculator.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CafeX.OrderCalculator
 {
     public class OrderCalculator
     {
+        private Dictionary<string, decimal> _menu = new Dictionary<string, decimal>();
+
+        public OrderCalculator()
+        {
+            _menu.Add("Cola", (decimal).5);
+            _menu.Add("Coffee", (decimal)1);
+            _menu.Add("Cheese Sandwich", (decimal)2);
+            _menu.Add("Steak Sandwich", (decimal)4.5);
+        }
+
         public decimal Calculate(string[] order)
         {
             if (order == null)
@@ -11,7 +22,17 @@ namespace CafeX.OrderCalculator
                 throw new ArgumentNullException("order");
             }
 
-            return (decimal)3.5;
+            decimal result = 0;
+
+            foreach (var item in order)
+            {
+                if (_menu.ContainsKey(item))
+                {
+                    result += _menu[item];
+                }
+            }
+
+            return result;
         }
     }
 }
