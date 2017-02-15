85237bb6cbf968cc1dd34af89bfe98f369668340
Mark Taylor <mark.taylor@betfred.com>
2017-02-15 20:41:17 +0000

    Step 2 - Added cold food implementation

--- tests/CafeX.OrderCalculator.Tests/SurchargeCalculatorTests.cs
+++ tests/CafeX.OrderCalculator.Tests/SurchargeCalculatorTests.cs
@@ -40,5 +40,22 @@ namespace CafeX.OrderCalculator.Tests
 
             Assert.Equal(expectedValue, result);
         }
+
+        [Theory]
+        [InlineData(new string[] { "Cheese Sandwich" }, 1)]
+        public void Order_Cold_Food(string[] order, decimal expectedValue) 
+        {
+            var menu = new List<Item>();
+            menu.Add(new Item { Name ="Cola", Price = (decimal)0.5, Category = ItemCategory.Drink, Temperature = ItemTemperature.Cold} );
+            menu.Add(new Item { Name ="Coffee", Price = (decimal)1, Category = ItemCategory.Drink, Temperature = ItemTemperature.Hot} );
+            menu.Add(new Item { Name ="Cheese Sandwich", Price = (decimal)2, Category = ItemCategory.Food, Temperature = ItemTemperature.Cold} );
+            menu.Add(new Item { Name ="Steak Sandwich", Price = (decimal)4.5, Category = ItemCategory.Food, Temperature = ItemTemperature.Hot} );
+
+            var sut = new SurchargeCalculator(menu);
+
+            var result = sut.Calculate(0, order);
+
+            Assert.Equal(expectedValue, result);
+        }
     }
 }
