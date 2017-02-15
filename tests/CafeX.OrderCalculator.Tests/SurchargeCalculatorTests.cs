add9c9f0fa868f3b86af58d229e7bf6937b4e7e6
Mark Taylor <mark.taylor@betfred.com>
2017-02-15 20:56:08 +0000

    Step 2 - Added hot food ceiling test

--- tests/CafeX.OrderCalculator.Tests/SurchargeCalculatorTests.cs
+++ tests/CafeX.OrderCalculator.Tests/SurchargeCalculatorTests.cs
@@ -91,5 +91,22 @@ namespace CafeX.OrderCalculator.Tests
 
             Assert.Equal(expectedValue, result);
         }
+
+        [Theory]
+        [InlineData(new string[] { "Steak Sandwich" }, 20)]
+        public void Order_Hot_Food_Over_Ceiling(string[] order, decimal expectedValue) 
+        {
+            var menu = new List<Item>();
+            menu.Add(new Item { Name ="Cola", Price = (decimal)0.5, Category = ItemCategory.Drink, Temperature = ItemTemperature.Cold} );
+            menu.Add(new Item { Name ="Coffee", Price = (decimal)1, Category = ItemCategory.Drink, Temperature = ItemTemperature.Hot} );
+            menu.Add(new Item { Name ="Cheese Sandwich", Price = (decimal)2, Category = ItemCategory.Food, Temperature = ItemTemperature.Cold} );
+            menu.Add(new Item { Name ="Steak Sandwich", Price = (decimal)4.5, Category = ItemCategory.Food, Temperature = ItemTemperature.Hot} );
+
+            var sut = new SurchargeCalculator(menu);
+
+            var result = sut.Calculate((decimal)200, order);
+
+            Assert.Equal(expectedValue, result);
+        }
     }
 }
