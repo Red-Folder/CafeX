b0aa82b5bf688c89e343f712aa387dc9529216a7
Mark Taylor <mark.taylor@betfred.com>
2017-02-15 20:47:58 +0000

    Step 2 - Added cold food round up test

--- tests/CafeX.OrderCalculator.Tests/SurchargeCalculatorTests.cs
+++ tests/CafeX.OrderCalculator.Tests/SurchargeCalculatorTests.cs
@@ -57,5 +57,22 @@ namespace CafeX.OrderCalculator.Tests
 
             Assert.Equal(expectedValue, result);
         }
+
+        [Theory]
+        [InlineData(new string[] { "Cheese Sandwich" }, 1.01)]
+        public void Order_Cold_Food_With_Roundup(string[] order, decimal expectedValue) 
+        {
+            var menu = new List<Item>();
+            menu.Add(new Item { Name ="Cola", Price = (decimal)0.5, Category = ItemCategory.Drink, Temperature = ItemTemperature.Cold} );
+            menu.Add(new Item { Name ="Coffee", Price = (decimal)1, Category = ItemCategory.Drink, Temperature = ItemTemperature.Hot} );
+            menu.Add(new Item { Name ="Cheese Sandwich", Price = (decimal)2, Category = ItemCategory.Food, Temperature = ItemTemperature.Cold} );
+            menu.Add(new Item { Name ="Steak Sandwich", Price = (decimal)4.5, Category = ItemCategory.Food, Temperature = ItemTemperature.Hot} );
+
+            var sut = new SurchargeCalculator(menu);
+
+            var result = sut.Calculate((decimal)10.01, order);
+
+            Assert.Equal(expectedValue, result);
+        }
     }
 }
