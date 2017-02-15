831cc1fe1cf2327977fc39f76f3600226b5531cd
Mark Taylor <mark.taylor@betfred.com>
2017-02-15 20:53:11 +0000

    Step 2 - Added hot food test

--- tests/CafeX.OrderCalculator.Tests/SurchargeCalculatorTests.cs
+++ tests/CafeX.OrderCalculator.Tests/SurchargeCalculatorTests.cs
@@ -74,5 +74,22 @@ namespace CafeX.OrderCalculator.Tests
 
             Assert.Equal(expectedValue, result);
         }
+
+        [Theory]
+        [InlineData(new string[] { "Steak Sandwich" }, 2)]
+        public void Order_Hot_Food(string[] order, decimal expectedValue) 
+        {
+            var menu = new List<Item>();
+            menu.Add(new Item { Name ="Cola", Price = (decimal)0.5, Category = ItemCategory.Drink, Temperature = ItemTemperature.Cold} );
+            menu.Add(new Item { Name ="Coffee", Price = (decimal)1, Category = ItemCategory.Drink, Temperature = ItemTemperature.Hot} );
+            menu.Add(new Item { Name ="Cheese Sandwich", Price = (decimal)2, Category = ItemCategory.Food, Temperature = ItemTemperature.Cold} );
+            menu.Add(new Item { Name ="Steak Sandwich", Price = (decimal)4.5, Category = ItemCategory.Food, Temperature = ItemTemperature.Hot} );
+
+            var sut = new SurchargeCalculator(menu);
+
+            var result = sut.Calculate((decimal)10, order);
+
+            Assert.Equal(expectedValue, result);
+        }
     }
 }
