2d7128943869033783932b710b72835fcc122e24
Mark Taylor <mark.taylor@betfred.com>
2017-02-15 20:23:23 +0000

    Step 2 - Creating test for OrderCalculator

--- src/CafeX.OrderCalculator/OrderCalculator.cs
+++ src/CafeX.OrderCalculator/OrderCalculator.cs
@@ -6,14 +6,28 @@ namespace CafeX.OrderCalculator
 {
     public class OrderCalculator
     {
-        private IList<Item> _menu = new List<Item>();
+        private IItemCalculator _net;
 
         public OrderCalculator()
         {
-            _menu.Add(new Item { Name ="Cola", Price = (decimal)0.5, Category = ItemCategory.Drink, Temperature = ItemTemperature.Cold} );
-            _menu.Add(new Item { Name ="Coffee", Price = (decimal)1, Category = ItemCategory.Drink, Temperature = ItemTemperature.Hot} );
-            _menu.Add(new Item { Name ="Cheese Sandwich", Price = (decimal)2, Category = ItemCategory.Food, Temperature = ItemTemperature.Cold} );
-            _menu.Add(new Item { Name ="Steak Sandwich", Price = (decimal)4.5, Category = ItemCategory.Food, Temperature = ItemTemperature.Hot} );
+            _net = new ItemCalculator(GetMenu());
+        }
+
+        public OrderCalculator(IItemCalculator net)
+        {
+            _net = net;
+        }
+
+        private IList<Item> GetMenu()
+        {
+            var menu = new List<Item>();
+
+            menu.Add(new Item { Name ="Cola", Price = (decimal)0.5, Category = ItemCategory.Drink, Temperature = ItemTemperature.Cold} );
+            menu.Add(new Item { Name ="Coffee", Price = (decimal)1, Category = ItemCategory.Drink, Temperature = ItemTemperature.Hot} );
+            menu.Add(new Item { Name ="Cheese Sandwich", Price = (decimal)2, Category = ItemCategory.Food, Temperature = ItemTemperature.Cold} );
+            menu.Add(new Item { Name ="Steak Sandwich", Price = (decimal)4.5, Category = ItemCategory.Food, Temperature = ItemTemperature.Hot} );
+
+            return menu;
         }
 
         public decimal Calculate(string[] order)
